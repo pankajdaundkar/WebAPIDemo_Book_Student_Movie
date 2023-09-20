b23c88b22840f210ecd37beb55fee3e71f7d107b
pankajdaundkar <phd27993@gmail.com>
2023-09-20 23:10:47 +0530

    add file

---
+++ WebAPIDemo/Controllers/ProductController.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIDemo.Entities;
+using WebAPIDemo.Srevices;
+
+namespace WebAPIDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+       
+            private readonly IBookServices services;
+            public BookController(IBookServices services)
+            {
+                this.services = services;
+            }
+            // GET: api/<BookController>
+            [HttpGet]
+            [Route("GetBooks")]
+            public IActionResult Get()
+            {
+                try
+                {
+                    return new ObjectResult(services.GetBooks());
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(StatusCodes.Status204NoContent, ex.Message);
+                }
+            }
+
+            // GET api/<BookController>/5
+            [HttpGet]
+            [Route("GetBookById/{id}")]
+            public IActionResult Get(int id)
+            {
+                try
+                {
+                    return new ObjectResult(services.GetBookById(id));
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(StatusCodes.Status204NoContent, ex.Message);
+                }
+
+            }
+
+            // POST api/<BookController>
+            [HttpPost]
+            [Route("AddBook")]
+            public IActionResult Post([FromBody] Book book)
+            {
+                try
+                {
+                    int result = services.AddBook(book);
+                    if (result >= 1)
+                    {
+                        return StatusCode(StatusCodes.Status201Created);
+
+                    }
+                    else
+                    {
+                        return StatusCode(StatusCodes.Status500InternalServerError);
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+
+                    return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+
+                }
+            }
+
+
+            // PUT api/<BookController>/5
+            [HttpPut]
+            [Route("UpdateBook")]
+            public IActionResult Put(int id, [FromBody] Book book)
+            {
+                try
+                {
+                    int result = services.AddBook(book);
+                    if (result >= 1)
+                    {
+                        return StatusCode(StatusCodes.Status200OK);
+
+                    }
+                    else
+                    {
+                        return StatusCode(StatusCodes.Status500InternalServerError);
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+
+                    return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+
+                }
+            }
+
+            // DELETE api/<BookController>/5
+            [HttpDelete]
+            [Route("DeleteBook/{id}")]
+            public IActionResult Delete(int id)
+            {
+                try
+                {
+                    int result = services.DeleteBook(id);
+                    if (result >= 1)
+                    {
+                        return StatusCode(StatusCodes.Status200OK);
+
+                    }
+                    else
+                    {
+                        return StatusCode(StatusCodes.Status500InternalServerError);
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+
+                    return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+
+                }
+
+            }
+        }
+}
---
+++ WebAPIDemo/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace WebAPIDemo.Entities
+{
+    [Table("Product")]
+    public class Product
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public int Price { get; set; }
+       
+        public string? Cname { get; set; }
+    }
+}
